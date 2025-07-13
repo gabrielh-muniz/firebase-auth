f5e73ba708e6cf83d8ccc1f6a94309d83a24e254
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-13 10:21:58 -0300

    fix: change the components to follow the auth state of the App.jsx

--- frontend/src/components/UserAuthControl.jsx
+++ frontend/src/components/UserAuthControl.jsx
@@ -2,7 +2,9 @@ import { useAuthStore } from "../store/authStore.js";
 import { Navigate } from "react-router-dom";
 
 export default function UserAuthControl({ children }) {
-  const { isAuthenticated } = useAuthStore();
+  const { isAuthenticated, isCheckingAuth } = useAuthStore();
+
+  if (isCheckingAuth) return <div>Loading...</div>;
 
   return isAuthenticated ? <Navigate to="/dashboard" replace /> : children;
 }
