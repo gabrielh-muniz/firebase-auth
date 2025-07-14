51045769f3f010a44ce411ea632c0e3369b24c3a
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-14 10:43:03 -0300

    feat: create a verification link for the user

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -5,36 +5,51 @@ import { query } from "../db/connection.js";
 
 export const router = Router();
 
-router.post("/register", verifyTokenId, verifyUserRole, async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/register",
+  /*verifyTokenId, verifyUserRole,*/ async (req, res) => {
+    const { email, password } = req.body;
 
-  if (!email || !password)
-    return res.status(400).json({ message: "Email and password are required" });
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
 
-  //TODO: verify if user already exists in the database
+    //TODO: verify if user already exists in the database
 
-  try {
-    const userRecord = await admin.auth().createUser({ email, password });
+    try {
+      const userRecord = await admin.auth().createUser({ email, password });
 
-    await query(
-      "INSERT INTO users (uid, email, display_name, roles) VALUES ($1, $2, $3, $4)",
-      [userRecord.uid, email, userRecord.displayName, ["admin"]]
-    );
+      await query(
+        "INSERT INTO users (uid, email, display_name, roles) VALUES ($1, $2, $3, $4)",
+        [userRecord.uid, email, userRecord.displayName, ["admin"]]
+      );
 
-    //TODO: send verification email
+      //TODO: send verification email
+      const actionCodeSettings = {
+        url: `${process.env.FRONTEND_URL}/verify-email`,
+      };
 
-    return res.status(201).json({
-      message: "User registered successfully",
-      user: {
-        uid: userRecord.uid,
-        email: userRecord.email,
-        displayName: userRecord.displayName,
-        emailVerified: userRecord.emailVerified,
-      },
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: `Internal server error: ${error.message}` });
+      const verificationLink = await admin
+        .auth()
+        .generateEmailVerificationLink(email, actionCodeSettings);
+
+      // TODO: send this link via email to the user with Resend
+
+      return res.status(201).json({
+        message: "User registered successfully",
+        verificationLink,
+        user: {
+          uid: userRecord.uid,
+          email: userRecord.email,
+          displayName: userRecord.displayName,
+          emailVerified: userRecord.emailVerified,
+        },
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Internal server error: ${error.message}` });
+    }
   }
-});
+);
