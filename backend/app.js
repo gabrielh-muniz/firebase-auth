67281244856773d6bb24b871fc6f644ae407b542
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-14 12:54:57 -0300

    feat: add logic to create a user in firebase linking with database

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,6 @@ app.use(express.json());
 
 app.use("/api/auth", userRouter);
 
-app.get("/", async (req, res) => {
-  const result = await query("SELECT NOW()");
-  res.send({ date: `${result.rows[0].now}` });
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,15 +1,45 @@
 import { Router } from "express";
 import { verifyTokenId } from "../middlewares/auth.js";
+import admin from "../config/firebase.js";
+import { query } from "../db/connection.js";
 
 export const router = Router();
 
-router.post("/register", verifyTokenId, (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/register",
+  /*verifyTokenId, */ async (req, res) => {
+    const { email, password } = req.body;
 
-  if (!email || !password)
-    return res.status(400).json({ message: "Email and password are required" });
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
 
-  // TODO: create a user in firebase and the database
+    //TODO: verify if user already exists in the database
 
-  // TODO: send a verification email
-});
+    try {
+      const userRecord = await admin.auth().createUser({ email, password });
+
+      await query(
+        "INSERT INTO users (uid, email, display_name, roles) VALUES ($1, $2, $3, $4)",
+        [userRecord.uid, email, userRecord.displayName, ["admin"]]
+      );
+
+      //TODO: send verification email
+
+      return res.status(201).json({
+        message: "User registered successfully",
+        user: {
+          uid: userRecord.uid,
+          email: userRecord.email,
+          displayName: userRecord.displayName,
+          emailVerified: userRecord.emailVerified,
+        },
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Internal server error: ${error.message}` });
+    }
+  }
+);
