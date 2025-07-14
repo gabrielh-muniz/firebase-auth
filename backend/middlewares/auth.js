bcf1b63052de9fb3a89b975290dc9a942a697b02
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-14 10:02:36 -0300

    feat: add a middleware to verify if the user is an admin or not

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 import admin from "firebase-admin";
+import { query } from "../db/connection.js";
 
 export async function verifyTokenId(req, res, next) {
   const authHeader = req.headers.authorization || "";
@@ -14,5 +15,29 @@ export async function verifyTokenId(req, res, next) {
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+export async function verifyUserRole(req, res, next) {
+  const { uid } = req.user;
+
+  try {
+    const result = await query("SELECT roles FROM users WHERE uid = $1", [uid]);
+    if (result.rows.length === 0)
+      return res.status(403).json({ message: "Forbidden: User not found" });
+
+    const roles = result.rows[0].roles;
+
+    if (!roles.includes("admin")) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: Insufficient permissions" });
+    }
+
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Internal server error: ${error.message}` });
   }
 }
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,45 +1,40 @@
 import { Router } from "express";
-import { verifyTokenId } from "../middlewares/auth.js";
+import { verifyTokenId, verifyUserRole } from "../middlewares/auth.js";
 import admin from "../config/firebase.js";
 import { query } from "../db/connection.js";
 
 export const router = Router();
 
-router.post(
-  "/register",
-  /*verifyTokenId, */ async (req, res) => {
-    const { email, password } = req.body;
+router.post("/register", verifyTokenId, verifyUserRole, async (req, res) => {
+  const { email, password } = req.body;
 
-    if (!email || !password)
-      return res
-        .status(400)
-        .json({ message: "Email and password are required" });
+  if (!email || !password)
+    return res.status(400).json({ message: "Email and password are required" });
 
-    //TODO: verify if user already exists in the database
+  //TODO: verify if user already exists in the database
 
-    try {
-      const userRecord = await admin.auth().createUser({ email, password });
+  try {
+    const userRecord = await admin.auth().createUser({ email, password });
 
-      await query(
-        "INSERT INTO users (uid, email, display_name, roles) VALUES ($1, $2, $3, $4)",
-        [userRecord.uid, email, userRecord.displayName, ["admin"]]
-      );
+    await query(
+      "INSERT INTO users (uid, email, display_name, roles) VALUES ($1, $2, $3, $4)",
+      [userRecord.uid, email, userRecord.displayName, ["admin"]]
+    );
 
-      //TODO: send verification email
+    //TODO: send verification email
 
-      return res.status(201).json({
-        message: "User registered successfully",
-        user: {
-          uid: userRecord.uid,
-          email: userRecord.email,
-          displayName: userRecord.displayName,
-          emailVerified: userRecord.emailVerified,
-        },
-      });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: `Internal server error: ${error.message}` });
-    }
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        uid: userRecord.uid,
+        email: userRecord.email,
+        displayName: userRecord.displayName,
+        emailVerified: userRecord.emailVerified,
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Internal server error: ${error.message}` });
   }
-);
+});
